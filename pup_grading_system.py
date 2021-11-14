32fce585bdffb9c315b39616e43c62214d585fed
jendlrs <jensennics.dlrs@gmail.com>
2021-11-14 14:28:43 +0800

    Added round function

--- pup_grading_system.py
+++ pup_grading_system.py
@@ -23,54 +23,54 @@
 #Description: Very Good
 
 #Step 1: ask for student grade in percentage, convert, and store
-# Does not round off the grades with decimals yet; only run whole numbers
-grade= float(input("Enter grade percentage: "))
+# Can round off grades
+grade_= float(input("Enter grade percentage: ")) 
+roundOffGrade = round (grade_)
 
 #Step 2: test if 1.0 
-if grade >=97 and grade <=100:
+if roundOffGrade >=97 and roundOffGrade <=100:
     print("Grade mark: 1.00\nDesription: Excellent")
 #Step 3: test if 1.25
-elif grade >=94 and grade <=96:
+elif roundOffGrade >=94 and roundOffGrade <=96:
     print("Grade mark: 1.25\nDesription: Excellent")
 #Step 4: test if 1.5
-elif grade >=91 and grade <=93:
+elif roundOffGrade >=91 and roundOffGrade <=93:
     print("Grade mark: 1.50\nDesription: Very Good")
 #Step 3: test if 1.75
-elif grade >=88 and grade <=90:
+elif roundOffGrade >=88 and roundOffGrade <=90:
     print("Grade mark: 1.75\nDesription: Very Good")
 #Step 4: test if 2.0
-elif grade >=85 and grade <=87:
+elif roundOffGrade >=85 and roundOffGrade <=87:
     print("Grade mark: 2.00\nDesription: Good")
 #Step 3: test if 2.25
-elif grade >=82 and grade <=84:
+elif roundOffGrade >=82 and roundOffGrade <=84:
     print("Grade mark: 2.25\nDesription: Good")
 #Step 4: test if 2.5
-elif grade >=79 and grade <=81:
+elif roundOffGrade >=79 and roundOffGrade <=81:
     print("Grade mark: 2.5\nDesription: Satisfactory")
 #Step 3: test if 2.75
-elif grade >=76 and grade <=78:
+elif roundOffGrade >=76 and roundOffGrade <=78:
     print("Grade mark: 2.75\nDesription: Satisfactory")
 #Step 4: test if 3.0
-elif grade == 75:
+elif roundOffGrade == 75:
     print("Grade mark: 3.00\nDesription: Passing")
 #Step 5: test if 5.0
-elif grade >=65 and grade <=74:
+elif roundOffGrade >=65 and roundOffGrade <=74:
     print("Grade mark: 5.00\nDesription: Failure")
 #Step 6: test if incomplete, withdrawn, dropped 
 else:
     print("It looks like the grade you entered do not meet the minimum grade percentage to either pass or have a failure mark.\nPlease answer the following questions to know if you are Incomplete, Withdrawn or Dropped.")
-    majorExam=input("Have you take your major exam?\nType Y if Yes and N if No: ")
-    withdrawn= input ("Have you written a letter to withdraw your enrollment?\nType Y if Yes and N if No: ")
-    absent= input ("Have you absent 10 times in the semester?\nType Y if Yes and N if No: ")
+    majorExam=input("1. Have you take your major exam?\nType Y if Yes and N if No: ")
+    withdrawn= input ("2. Have you written a letter to withdraw your enrollment?\nType Y if Yes and N if No: ")
+    absent= input ("3. Have you absent 10 times in the semester?\nType Y if Yes and N if No: ")
 
     if (majorExam== "N" and withdrawn == "N") and (absent=="N"):
         print("Grade Mark: Inc.\nDescription: Incomplete")
     elif (majorExam== "Y") and (withdrawn == "N" or absent== "N"):
-        print("It looks like there is a problem occured.\nPlease ask your teacher for clarification")
+        print("It looks like there is a problem occured.\nPlease ask your teacher for clarification.")
     elif majorExam== "N" and withdrawn == "Y":
         print("Grade Mark: W\nDescription: Withdrawn")
     else:
         print("Grade Mark: D\nDescription: Dropped\nIt looks like your teacher gave you a dropped mark for exceeding\nthe allowable number of absences or for not attending class since the start of the term.")
-
 
 
