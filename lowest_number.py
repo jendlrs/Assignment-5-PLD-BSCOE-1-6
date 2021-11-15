f39ef8a74070b6eafe3e78eaeb71f171564f8021
jendlrs <jensennics.dlrs@gmail.com>
2021-11-15 18:06:36 +0800

    Ask users for users to input 3 numbers.

--- lowest_number.py
+++ lowest_number.py
@@ -4,4 +4,12 @@
 #Display the lowest number
 
 #Step 1: ask for 3 numbers convert, and store
+def getnumbers():
+    firstN= int(input("Enter first number: "))
+    secondN= int(input("Enter second number: "))
+    thirdN= int(input ("Enter third number: "))
+    return firstN, secondN, thirdN
 #Step 2: test the 3 numbers to find the lowest number
+
+
+first, second, third = getnumbers()
