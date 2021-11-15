06397ed43989f85ad4485d321f2d033a1ed66f4b
jendlrs <jensennics.dlrs@gmail.com>
2021-11-15 18:09:58 +0800

    I added if-elif-else statement to find the lowest number.

--- lowest_number.py
+++ lowest_number.py
@@ -10,6 +10,13 @@ def getnumbers():
     thirdN= int(input ("Enter third number: "))
     return firstN, secondN, thirdN
 #Step 2: test the 3 numbers to find the lowest number
-
+def lowestN(firstNA,secondNA,thirdNA):
+    if firstNA < secondNA and firstNA <thirdNA:
+        print(f"The lowest number is {firstNA}")
+    elif secondNA < firstNA and secondNA <thirdNA:
+        print(f"The lowest number is {secondNA}")
+    else:
+        print(f"The lowest number is {thirdNA}")
 
 first, second, third = getnumbers()
+lowestN(first, second, third)
