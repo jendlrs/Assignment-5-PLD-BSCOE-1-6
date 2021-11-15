373cc48ec53ba4d215d8e7e28530aafffebe1a3b
jendlrs <jensennics.dlrs@gmail.com>
2021-11-15 18:02:57 +0800

    I listed the steps I needed for the program

---
+++ lowest_number.py
@@ -0,0 +1,7 @@
+#Program 2: Find the lowest number
+#Create a program that ask 3 numbers. 
+#Find the lowest number using only if-else statement.
+#Display the lowest number
+
+#Step 1: ask for 3 numbers convert, and store
+#Step 2: test the 3 numbers to find the lowest number
