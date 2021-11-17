d4caacaff809c4165b1ae5aed223a96fc9f7d239
jendlrs <jensennics.dlrs@gmail.com>
2021-11-17 15:18:57 +0800

    The code is completed, I check the grammar of some sentences.

--- pup_grading_system.py
+++ pup_grading_system.py
@@ -1,86 +1,64 @@
 #Program 1: PUP Grading System
-#Section 8 of https://www.pup.edu.ph/studentservices/files/ThePUPStudentHandbook2014.pdf
-   
-    # grade/mark   Percentage      Description
-    # 1.0           97-100          Excellent
-    # 1.25          94-96           Excellent
-    # 1.5           91-93           Very Good
-    # 1.75          88-90           Very Good
-    # 2.0           85-87           Good
-    # 2.25          82-84           Good
-    # 2.5           79-81           Satisfactory
-    # 2.75          76-78           Satisfactory
-    # 3.0           75              Passing
-    # 5.0           65-74           Failure
-    # Inc.                          Incomplete
-    # W                             Withdrawn
-    # D                             Dropped
-
 #Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
-#Example:
-#Input grade: 87.6
-#Grade/Mark: 1.75
-#Description: Very Good
 
 #Step 1: ask for student grade in percentage, convert, and store
 # Can round off grades
-print ("Polytechnic University of the Philippines Grading System")
-print("Please enter your grade percentage here. Type 'N' if you do not attained any grade.")
+print ("\nPolytechnic University of the Philippines Grading System")
+print("\nPlease enter your grade percentage here. Type 'N' if you do not attained any grade.")
 
 while True:
     try:
         quest_grade= float(input("Answer:  ")) 
         roundOffGrade = round(quest_grade)
-        print(roundOffGrade)
 
 #Step 2: test if 1.0 
         if roundOffGrade >=97 and roundOffGrade <=100:
-            print("Grade mark: 1.00\nDesription: Excellent")
+            print("\nGrade/Mark: 1.00\nDesription: Excellent\n")
 #Step 3: test if 1.25
         elif roundOffGrade >=94 and roundOffGrade <=96:
-            print("Grade mark: 1.25\nDesription: Excellent")
+            print("\nGrade/Mark: 1.25\nDesription: Excellent\n")
 #Step 4: test if 1.5
         elif roundOffGrade >=91 and roundOffGrade <=93:
-            print("Grade mark: 1.50\nDesription: Very Good")
+            print("\nGrade/Mark: 1.50\nDesription: Very Good\n")
 #Step 3: test if 1.75
         elif roundOffGrade >=88 and roundOffGrade <=90:
-            print("Grade mark: 1.75\nDesription: Very Good")
+            print("\nGrade/Mark: 1.75\nDesription: Very Good\n")
 #Step 4: test if 2.0
         elif roundOffGrade >=85 and roundOffGrade <=87:
-            print("Grade mark: 2.00\nDesription: Good")
+            print("\nGrade/Mark: 2.00\nDesription: Good\n")
 #Step 3: test if 2.25
         elif roundOffGrade >=82 and roundOffGrade <=84:
-            print("Grade mark: 2.25\nDesription: Good")
+            print("\nGrade/Mark: 2.25\nDesription: Good\n")
 #Step 4: test if 2.5
         elif roundOffGrade >=79 and roundOffGrade <=81:
-            print("Grade mark: 2.5\nDesription: Satisfactory")
+            print("\nGrade/Mark: 2.5\nDesription: Satisfactory\n")
 #Step 3: test if 2.75
         elif roundOffGrade >=76 and roundOffGrade <=78:
-            print("Grade mark: 2.75\nDesription: Satisfactory")
+            print("\nGrade/Mark: 2.75\nDesription: Satisfactory\n")
 #Step 4: test if 3.0
         elif roundOffGrade == 75:
-            print("Grade mark: 3.00\nDesription: Passing")
+            print("\nGrade/Mark: 3.00\nDesription: Passing\n")
 #Step 5: test if 5.0
         elif roundOffGrade >=65 and roundOffGrade <=74:
-            print("Grade mark: 5.00\nDesription: Failure")
+            print("\nGrade/Mark: 5.00\nDesription: Failure\n")
 #Step 6: test if incomplete, withdrawn, dropped 
         else:
-            print("The grade you entered is invalid. Grade percentage must be in positive numbers and not greater than 100.")
-
+            print("\nThe grade you entered is invalid. Grade percentage must be in positive numbers and not greater than 100.\n")
+        break
     except ValueError:
         #The following questions is also connected to Section 8 "Grading System"
 
-        print("Please answer the following questions to know if your status is Incomplete, Withdrawn or Dropped.")
-        majorExam=input("1. Have you take your major exam?\nType Y if Yes and N if No: ")
-        withdrawn= input ("2. Have you written a letter to withdraw your enrollment?\nType Y if Yes and N if No: ")
-        absent= input ("3. Have you absent 10 times in the semester?\nType Y if Yes and N if No: ")
+        print("\nPlease answer the following questions to know if your status is Incomplete, Withdrawn or Dropped.")
+        majorExam=input("\n1. Have you taken your major exam?\nType Y if Yes and N if No: ")
+        withdrawn= input ("\n2. Have you written a letter to withdraw your enrollment?\nType Y if Yes and N if No: ")
+        absent= input ("\n3. Have you been absent ten (10) times in the semester?\nType Y if Yes and N if No: ")
     
         if (majorExam== "N" and withdrawn == "N") and (absent=="N"):
-            print("Grade Mark: Inc.\nDescription: Incomplete.\n")
+            print("\nGrade Mark: Inc.\nDescription: Incomplete.\n")
         elif (majorExam== "Y") and (withdrawn == "N" or absent== "N"):
-            print("It looks like there is a problem occured.\nPlease ask your teacher for clarification.")
+            print("\nIt looks like there is a problem occurred.\nPlease ask your teacher for clarification.\n")
         elif majorExam== "N" and withdrawn == "Y":
-            print("Grade Mark: W\nDescription: Withdrawn\nA Withdrawn mark is given if the student voluntarily withdraws in writing from a subject\nat any time but not less than one (1) month before the final examination")
+            print("\nGrade Mark: W\nDescription: Withdrawn\n\nA Withdrawn Mark is given when the student voluntarily withdraws in writing from a subject\nat any time but not less than one (1) month before the final examination\n")
         else:    
-            print("Grade Mark: D\nDescription: Dropped\nYour teacher gave you a dropped mark for exceeding\nthe allowable number of absences or for not attending class since the start of the term.")
+            print("\nGrade Mark: D\nDescription: Dropped\n\nYour teacher gave you a dropped Mark for exceeding the allowable number\nof absences or for not attending class since the start of the term.\n")
         break
