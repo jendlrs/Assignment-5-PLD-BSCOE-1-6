2e315d8491179c76fe30b0e322e96c42486b3874
jendlrs <jensennics.dlrs@gmail.com>
2021-11-19 14:29:31 +0800

    I listed the steps and informationn program before start coding.

---
+++ the_life_stages.py
@@ -0,0 +1,15 @@
+#Program 3: Life stages
+#Create a program that ask for an age of a person
+#Display the life stage of the person.
+#Rules:
+#0 - 12 : Kid
+#13 - 17 : Teen
+#18 : Debut
+#19 above : Adult
+
+#Step 1: ask for student grade in percentage, convert, and store
+#Step 2: test if kid
+#Step 3: test if teen
+#Step 4: test if debut
+#test if adult
+
