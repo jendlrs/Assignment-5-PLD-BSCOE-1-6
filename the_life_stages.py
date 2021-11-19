c252e120062bcc7798e83a5fecdb648533221c35
jendlrs <jensennics.dlrs@gmail.com>
2021-11-19 15:06:19 +0800

    the if-else statement for step 2: test if kid is done.

--- the_life_stages.py
+++ the_life_stages.py
@@ -11,16 +11,21 @@
 def getAge():
     while True:
         try:
-            age_= int(input("How old are you? "))
+            age_= int(input("\nHow old are you? "))
+            #Step 2: test if kid
+            if age_ >=0 and age_ <=12:
+                print(f"\nHi, user! Because you are {age_} years old, you are still a Kid.\n")
+            else:
+                print("Others")
+            #Step 3: test if teen
+            #Step 4: test if debut
+            #Step 5: test if adult
             break
         except ValueError:
-            print("The age you have entered is invalid. Please enter your age in numbers")
+            print("\nThe age you have entered is invalid. Please enter your age in numbers")
             continue
     return age_
 
 age= getAge()
-#Step 2: test if kid
-#Step 3: test if teen
-#Step 4: test if debut
-#test if adult
+
 
