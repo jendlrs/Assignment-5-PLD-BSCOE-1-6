a1cbfd9cdef9660aa78e352e9e6799ad06312d5e
jendlrs <jensennics.dlrs@gmail.com>
2021-11-19 15:32:36 +0800

    The code is complete. I aldo added introduction to greet the users.

--- the_life_stages.py
+++ the_life_stages.py
@@ -7,6 +7,7 @@
 #18 : Debut
 #19 above : Adult
 
+print("\nWelcome! This program will tell you what phase of life you are in the present.")
 name_= input("\nWhat is your name? ")
 #Step 1: ask for an age of the person
 def getAge():
@@ -21,10 +22,10 @@ def getAge():
                 print(f"\nHi, {name_}! Because you are {age_} years old, you are now a Teen.\n")
             #Step 4: test if debut
             elif age_ == 18:
-                print(f"\nHi, {name_}! Because you are {age_}, you are a Debutant.\n")
+                print(f"\nHi, {name_}! Because you are {age_} years old, you are a Debutant.\n")
             #Step 5: test if adult
             elif age_ >=19:
-                print(f"\nHi, {name_}! Because you are 19 years old, you are an Adult.\n")
+                print(f"\nHi, {name_}! Because you are {age_} years old, you are an Adult.\n")
             else:
                 print("\nThere is no such age in ngative numbers. Please try again.")
                 continue
@@ -32,7 +33,7 @@ def getAge():
         except ValueError:
             print("\nThe age you have entered is invalid. Please enter your age in numbers")
         continue
-
+    
 age= getAge()
 
 
